c8c3ab1891ed6def2a9f111a0fb3a77a28b6cbd0
Nathaniel Reeder <n.ash.reeder@gmail.com>
2023-03-08 15:15:14 -0700

    Lab completed

---
+++ main.js
@@ -0,0 +1,26 @@
+const getResidentsBtn = document.querySelector("#get-residents")
+
+
+
+
+const buttonClicked = () => console.log("The 'get residents' button has been clicked")
+
+const createNameElement = (residentObj) => {
+    const h2 = document.createElement("h2")
+    const name = document.createTextNode(`${residentObj.data.name}`.toLowerCase())
+    h2.appendChild(name)
+    const element = document.getElementById("name-area")
+    element.appendChild(h2)
+}
+
+const residentURL = (residentURL) => {
+    axios.get(`${residentURL}`).then(createNameElement)
+}
+
+const getResidentCallback = (searchResult) => {
+    let residents = searchResult.data.results[0].residents
+    residents.forEach(residentURL)
+}
+
+const getResidents = () => axios.get("https://swapi.dev/api/planets/?search=alderaan").then(getResidentCallback).catch((err) => console.log(err))
+getResidentsBtn.addEventListener('click', getResidents)
